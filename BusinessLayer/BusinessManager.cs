c13e51eb4cfab17361151a37e07e7bdc10510569
Joo70 <jordanbarret@orange.fr>
2017-02-03 14:06:33 +0100

    Add files via upload

---
+++ BusinessLayer/BusinessManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EntitiesLayer;
+using StubDataAccessLayer;
+
+namespace BusinessLayer
+{
+    public class BusinessManager
+    {
+        static private DalManager Manager = new DalManager();
+
+        public BusinessManager() {
+        }
+
+        public List<Pokemon> getPokemon() {
+            return Manager.LsPokemon;
+        }
+
+        public List<Stade> getStade() {
+                return Manager.LsStade;
+        }
+
+        public List<Match> getMatch() {
+            return Manager.LsMatch;
+        }
+
+        public List<String> getPokemonByElem(ETypeElement elem) {
+			var query = from pokemonElem in Manager.getPokemonByElement(elem)
+						select pokemonElem.Nom;
+			return query.ToList();
+		}
+
+		public List<String> getPokemonForceVie() {
+			var query = from pokemon in Manager.LsPokemon
+						where pokemon.carac.Vie > 30 &&
+							  pokemon.carac.Force > 3
+						select pokemon.Nom;
+			return query.ToList();
+		}
+
+        public bool CheckConnexionUser(string login, string password)
+        {
+            bool verif=false;
+            try
+            {
+                Utilisateur user = Manager.GetUtilisateurByLogin(login);
+                if (user != null)
+                {
+                    if (user.getPassword() == password)
+                    {
+                        verif = true;
+                    }
+                }
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return verif;
+        }
+    }
+}
